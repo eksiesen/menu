97df7f10eaa27d68c6a7dd1942deb11564fb9745
aalptugdemir <alialptugdemir@gmail.com>
2023-11-15 22:50:38 +0300

    Comment lines added.

--- src/agriculture/src/agriculture.cpp
+++ src/agriculture/src/agriculture.cpp
@@ -5,21 +5,25 @@
 #include <string>
 #include <stack>
 
-
+// Calculate and return the percentage increase in sales for the AgriculturalProduct instance.
 double AgriculturalProduct::calculate_sales_increase() const {
     return ((updated_sales - previous_sales) / static_cast<double>(previous_sales)) * 100;
 }
 
 // Implement other functions from AgricultureMarket and SupplierCustomerManagement here
 
-
+// Default constructor for SupplierCustomerManagement, initializes reliability_score to 0.
 SupplierCustomerManagement::SupplierCustomerManagement() : reliability_score(0) {}
 
+// Parameterized constructor for SupplierCustomerManagement, setting attributes based on input.
 SupplierCustomerManagement::SupplierCustomerManagement(const std::string& n, int score, const std::string& fb, const std::string& ct)
     : name(n), reliability_score(score), feedback(fb), contract(ct) {}
 
+// Prompt the user to add product and market information to the AgricultureMarket instance.
 void AgricultureMarket::add_product_and_market_information() {
     AgriculturalProduct product;
+
+    // Input product details from the user.
     std::cout << "Enter product name: ";
     std::cin >> product.name;
     std::cout << "Enter previous price: ";
@@ -37,10 +41,11 @@ void AgricultureMarket::add_product_and_market_information() {
     std::cout << "Does the product have GMO? (1 for yes, 0 for no): ";
     std::cin >> product.has_gmo;
 
+    // Add the product to the products vector.
     products.push_back(product);
     std::cout << "Product and market information added successfully.\n";
 
-    // Dosyaya yazma iþlemi
+    // Write product information to a file.
     std::ofstream file("agriculture.txt", std::ios::app);
     if (file.is_open()) {
         file << "Product Name: " << product.name << "\n";
@@ -59,11 +64,13 @@ void AgricultureMarket::add_product_and_market_information() {
 }
 
 
+// Update the information of a product in the AgricultureMarket instance.
 void AgricultureMarket::update_product() {
     std::cout << "Enter the product name to update information: ";
     std::string product_name;
     std::cin >> product_name;
 
+    // Search for the product and update its information if found.
     for (auto& product : products) {
         if (product.name == product_name) {
             std::cout << "Enter updated price: ";
@@ -82,14 +89,17 @@ void AgricultureMarket::update_product() {
         }
     }
 
+    // Inform the user if the product is not found.
     std::cout << "Product not found.\n";
 }
 
+// Delete a product from the AgricultureMarket instance based on user input.
 void AgricultureMarket::delete_product() {
     std::cout << "Enter the product name to delete: ";
     std::string product_name;
     std::cin >> product_name;
 
+    // Use remove_if and erase to delete the product from the vector.
     auto it = std::remove_if(products.begin(), products.end(),
         [product_name](const AgriculturalProduct& product) {
             return product.name == product_name;
@@ -104,18 +114,21 @@ void AgricultureMarket::delete_product() {
     }
 }
 
+// List market information for a specific product based on user input.
 void AgricultureMarket::list_market_information() const {
     std::cout << "Enter the product name to list market information: ";
     std::string product_name;
     std::cin >> product_name;
     std::stack<AgriculturalProduct> productStack;
 
+    // Search for the product and push it onto the stack if found.
     for (const auto& product : products) {
         if (product.name == product_name) {
             productStack.push(product);
         }
     }
 
+    // Display market information for the top product on the stack.
     if (!productStack.empty()) {
         AgriculturalProduct product = productStack.top();
         productStack.pop();
@@ -137,9 +150,10 @@ void AgricultureMarket::list_market_information() const {
     }
 }
 
+// List all products and their information in the AgricultureMarket instance.
 void AgricultureMarket::list_products() const {
     std::cout << "List of Products:\n";
-    // Dosyadan okuma iþlemi
+    // Read product information from a file.
     std::ifstream file("agriculture.txt");
     if (file.is_open()) {
         std::string line;
@@ -152,6 +166,7 @@ void AgricultureMarket::list_products() const {
         std::cerr << "Unable to open file for reading.\n";
     }
 
+    // Display information for each product in the vector.
     for (const auto& product : products) {
         std::cout << "Product Name: " << product.name << "\n";
         std::cout << "Previous Price: " << product.previous_price << "\n";
@@ -165,6 +180,7 @@ void AgricultureMarket::list_products() const {
     }
 }
 
+// Add a new supplier or customer to the AgricultureMarket instance.
 void AgricultureMarket::add_supplier_customer() {
     SupplierCustomerManagement scm;
     std::cout << "Enter name: ";
@@ -176,10 +192,12 @@ void AgricultureMarket::add_supplier_customer() {
     std::cout << "Enter contract: ";
     std::cin >> scm.contract;
 
+    // Add the new supplier/customer to the vector.
     suppliers_customers.push_back(scm);
     std::cout << "Supplier/Customer added successfully.\n";
 }
 
+// List all suppliers and customers in the AgricultureMarket instance.
 void AgricultureMarket::list_suppliers_customers() const {
     std::cout << "List of Suppliers/Customers:\n";
     for (const auto& scm : suppliers_customers) {
@@ -190,11 +208,13 @@ void AgricultureMarket::list_suppliers_customers() const {
     }
 }
 
+// List sales information for a specific product based on user input.
 void AgricultureMarket::list_sales_information() const {
     std::cout << "Enter the product name to list sales information: ";
     std::string product_name;
     std::cin >> product_name;
 
+    // Search for the product and display its sales information if found.
     for (const auto& product : products) {
         if (product.name == product_name) {
             std::cout << "Sales information for " << product.name << ":\n";
@@ -213,6 +233,7 @@ void AgricultureMarket::list_sales_information() const {
     std::cout << "Product not found.\n";
 }
 
+// Display a menu for managing Agriculture and Agricultural Products Market and handle user choices.
 void AgricultureMarket::list_menu() {
     while (true) {
         std::cout << "\nAgriculture and Agricultural Products Market Menu:\n";
@@ -231,30 +252,39 @@ void AgricultureMarket::list_menu() {
 
         switch (choice) {
         case 1:
+            // Call the function to add a new supplier or customer.
             add_supplier_customer();
             break;
         case 2:
+            // Call the function to add product and market information.
             add_product_and_market_information();
             break;
         case 3:
+            // Call the function to update product information.
             update_product();
             break;
         case 4:
+            // Call the function to delete products.
             delete_product();
             break;
         case 5:
+            // Call the function to list all products.
             list_products();
             break;
         case 6:
+            // Call the function to list sales information for a specific product.
             list_sales_information();
             break;
         case 7:
+            // Call the function to list suppliers and customers.
             list_suppliers_customers();
             break;
         case 8:
+            // Display a message and exit the program.
             std::cout << "Exiting program.\n";
             return;
         default:
+            // Inform the user of an invalid choice.
             std::cout << "Invalid choice. Please enter a number between 1 and 9.\n";
         }
     }
